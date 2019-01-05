0d40e220c67576eb139ccd69304e482482d4f5ed
nardos-negussie <nardosnegussie519461@gmail.com>
2019-01-06 00:45:49 +0300

    setup main window

---
+++ outdoController.java
@@ -0,0 +1,36 @@
+package outdo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class outdoController {
+    @FXML private Button addTask;
+    @FXML private TextField taskField;
+    @FXML private VBox vb;
+    @FXML private ScrollPane sp;
+
+
+    @FXML
+    public void DoAddTask(ActionEvent event){
+//        HBox hb = new HBox();
+//
+//        CheckBox cb = new CheckBox();
+//        Button delete =  new Button("Delete");
+//        Button edit = new Button("Edit");
+//        Label task = new Label();
+//        task.setText(taskField.getText());
+//
+//
+//        hb.getChildren().addAll(cb, task, edit, delete);
+//        vb.getChildren().add(hb);
+
+    }
+
+}
